5c266557c399e420067acc900922d1248095f40b
Livio Ribeiro <livioribeiro@outlook.com>
2023-07-27 16:08:11 -0300

    disable woodpecker-ci

--- terraform/woodpecker-ci.tf
+++ terraform/woodpecker-ci.tf
@@ -1,109 +1,118 @@
-resource "nomad_namespace" "cicd" {
-  name = "system-cicd"
-}
+# resource "nomad_namespace" "cicd" {
+#   name = "system-cicd"
+# }
 
-resource "nomad_external_volume" "woodpecker_data" {
-  depends_on = [
-    data.nomad_plugin.nfs
-  ]
+# resource "nomad_external_volume" "woodpecker_data" {
+#   depends_on = [
+#     data.nomad_plugin.nfs
+#   ]
 
-  type         = "csi"
-  plugin_id    = "nfs"
-  volume_id    = "woodpecker-ci-data"
-  name         = "woodpecker-ci-data"
-  namespace    = nomad_namespace.cicd.name
-  capacity_min = "500MiB"
-  capacity_max = "750MiB"
+#   type         = "csi"
+#   plugin_id    = "nfs"
+#   volume_id    = "woodpecker-ci-data"
+#   name         = "woodpecker-ci-data"
+#   namespace    = nomad_namespace.cicd.name
+#   capacity_min = "500MiB"
+#   capacity_max = "750MiB"
 
-  parameters = {
-    uid  = "1000"
-    gid  = "1000"
-    mode = "770"
-  }
+#   parameters = {
+#     uid  = "1000"
+#     gid  = "1000"
+#     mode = "770"
+#   }
 
-  capability {
-    access_mode     = "single-node-writer"
-    attachment_mode = "file-system"
-  }
-}
+#   capability {
+#     access_mode     = "single-node-writer"
+#     attachment_mode = "file-system"
+#   }
+# }
 
-data "http" "woodpecker_ci_gitea_app_check" {
-  url = "http://root:Password123@gitea.${var.apps_subdomain}.${var.external_domain}/api/v1/user/applications/oauth2"
-  request_headers = {
-    "Accept": "application/json"
-  }
-}
+# data "http" "woodpecker_ci_gitea_app_check" {
+#   depends_on = [ nomad_job.gitea ]
 
-locals {
-  gitea_woodpecker_apps = [
-    for a in jsondecode(data.http.woodpecker_ci_gitea_app_check.response_body)
-    : a if a.name == "woodpecker-ci"
-  ]
-}
+#   url = "http://root:Password123@gitea.${var.apps_subdomain}.${var.external_domain}/api/v1/user/applications/oauth2"
+#   request_headers = {
+#     "Accept": "application/json"
+#   }
 
-locals {
-  gitea_woodpecker_app = length(local.gitea_woodpecker_apps) == 0 ? null : element(local.gitea_woodpecker_apps, 0)
-}
+#   request_timeout_ms = 10000
+#   retry {
+#     attempts = 30
+#     max_delay_ms = 10000
+#     min_delay_ms = 1000
+#   }
+# }
 
-data "external" "woodpecker_ci_gitea_app" {
-  program = [
-    "/bin/sh",
-    "-c",
-    <<EOT
-app='${jsonencode(local.gitea_woodpecker_app)}'
-result=''
-if [ "$app" = 'null' ]
-then
-  result=$(curl -u 'root:Password123' -H 'Content-Type: application/json' 'http://@gitea.${var.apps_subdomain}.${var.external_domain}/api/v1/user/applications/oauth2')
-else
-  result="$app"
-fi
+# locals {
+#   gitea_woodpecker_apps = [
+#     for a in jsondecode(data.http.woodpecker_ci_gitea_app_check.response_body)
+#     : a if a.name == "woodpecker-ci"
+#   ]
+# }
 
-result=$(echo $result | sed -e 's/"/\\\\"/g')
-echo "{\"result\": \"$result\"}"
-    EOT
-  ]
-}
+# locals {
+#   gitea_woodpecker_app = length(local.gitea_woodpecker_apps) == 0 ? null : element(local.gitea_woodpecker_apps, 0)
+# }
 
-locals {
-  woodpecker_gitea_app_result = jsondecode(data.external.woodpecker_ci_gitea_app.result.result)
-}
+# data "external" "woodpecker_ci_gitea_app" {
+#   program = [
+#     "/bin/sh",
+#     "-c",
+#     <<EOT
+# app='${jsonencode(local.gitea_woodpecker_app)}'
+# result=''
+# if [ "$app" = 'null' ]
+# then
+#   result=$(curl -u 'root:Password123' -H 'Content-Type: application/json' 'http://@gitea.${var.apps_subdomain}.${var.external_domain}/api/v1/user/applications/oauth2')
+# else
+#   result="$app"
+# fi
 
-locals {
-  gitea_woodpecker_client_id     = local.woodpecker_gitea_app_result.client_id
-  gitea_woodpecker_client_secret = local.woodpecker_gitea_app_result.client_secret
-}
+# result=$(echo $result | sed -e 's/"/\\\\"/g')
+# echo "{\"result\": \"$result\"}"
+#     EOT
+#   ]
+# }
 
-resource "nomad_job" "woodpecker" {
-  depends_on = [nomad_job.docker_registry]
+# locals {
+#   woodpecker_gitea_app_result = jsondecode(data.external.woodpecker_ci_gitea_app.result.result)
+# }
 
-  jobspec = file("${path.module}/jobs/woodpecker-ci.nomad.hcl")
-  # detach = false
+# locals {
+#   gitea_woodpecker_client_id     = local.woodpecker_gitea_app_result.client_id
+#   gitea_woodpecker_client_secret = local.woodpecker_gitea_app_result.client_secret
+# }
 
-  hcl2 {
-    enabled = true
-    vars = {
-      namespace               = nomad_namespace.cicd.name
-      data_volume_name        = nomad_external_volume.woodpecker_data.name
-      woodpecker_host         = "http://woodpecker.${var.apps_subdomain}.${var.external_domain}"
-      woodpecker_agent_secret = sha256("woodpecker-ci")
-      gitea_url               = "http://gitea.${var.apps_subdomain}.${var.external_domain}"
-      gitea_client            = local.gitea_woodpecker_client_id
-      gitea_secret            = local.gitea_woodpecker_client_secret
-    }
-  }
-}
+# resource "nomad_job" "woodpecker" {
+#   depends_on = [nomad_job.docker_registry]
 
-resource "consul_config_entry" "woodpecker_agent_intention" {
-  kind = "service-intentions"
-  name = "woodpecker-server"
+#   jobspec = file("${path.module}/jobs/woodpecker-ci.nomad.hcl")
+#   # detach = false
 
-  config_json = jsonencode({
-    Sources = [
-      {
-        Name   = "woodpecker-agent"
-        Action = "allow"
-      }
-    ]
-  })
-}
+#   hcl2 {
+#     enabled = true
+#     vars = {
+#       namespace               = nomad_namespace.cicd.name
+#       data_volume_name        = nomad_external_volume.woodpecker_data.name
+#       woodpecker_host         = "http://woodpecker.${var.apps_subdomain}.${var.external_domain}"
+#       woodpecker_agent_secret = sha256("woodpecker-ci")
+#       gitea_url               = "http://gitea.${var.apps_subdomain}.${var.external_domain}"
+#       gitea_client            = local.gitea_woodpecker_client_id
+#       gitea_secret            = local.gitea_woodpecker_client_secret
+#     }
+#   }
+# }
+
+# resource "consul_config_entry" "woodpecker_agent_intention" {
+#   kind = "service-intentions"
+#   name = "woodpecker-server"
+
+#   config_json = jsonencode({
+#     Sources = [
+#       {
+#         Name   = "woodpecker-agent"
+#         Action = "allow"
+#       }
+#     ]
+#   })
+# }
